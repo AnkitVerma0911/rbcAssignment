2f24592f975fe585e62fe81b3917f0effd32dc48
ankzzone <ankzzone@gmail.com>
2023-04-13 17:33:20 -0400

    first commit

---
+++ cSharpProgram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace rbcAssignment
+{
+    class cSharpProgram
+    {
+       public static void Main(string[] args)
+        {
+
+            Console.WriteLine(generatePattern(5));
+        }
+        public static String generatePattern(int patternSize)
+        {
+
+            String str = "";
+            char c;
+            for (int i = 0; i < patternSize; i++)
+            {
+                for (int j = 0; j < patternSize; j++)
+                {
+                    if (i == j || i + j == patternSize - 1)
+                    {
+                        c = '!';
+                    }
+                    else
+                    {
+                        c = '*';
+                    }
+                    str = str + c;
+                }
+                str = str + "\n";
+            }
+            return str;
+        }
+
+    }
+
+}
+
